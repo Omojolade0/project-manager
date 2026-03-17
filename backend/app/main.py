1c1a5cc7cb7ff8cce932b00a5be8cb6cafca3712
Omojolade0 <ipimidun@gmail.com>
2026-03-17 20:29:57 +0000

    fix cors for vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         os.getenv("FRONTEND_URL", ""),
+        "https://project-manager-psi-eight.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
